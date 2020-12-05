be03f1a7655c06a5a308a8642e1b8d4933195d01
Hasnat <61647150+hasnatosman@users.noreply.github.com>
2020-12-06 01:15:07 +0600

    Add files via upload
    using extended slicing.

---
+++ reverse_string3.py
@@ -0,0 +1,8 @@
+def reverse(string):
+    string = string[::-1]
+    return string
+
+
+string = 'HASNAT'
+print('The original string is :', string)
+print('The reversed string is :', reverse(string))
